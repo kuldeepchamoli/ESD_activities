8556c8002cc8f5fe1d28a49d8e9f2a35dcee44db
Kuldeep Chamoli <kuldeepchamoli889@gmail.com>
2024-11-12 13:33:31 +0530

    security is being added

--- springsec/src/main/java/com/kuldeepchamoli/springsec/Hellocontroller.java
+++ springsec/src/main/java/com/kuldeepchamoli/springsec/Hellocontroller.java
@@ -1,4 +1,12 @@
 package com.kuldeepchamoli.springsec;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Hellocontroller {
+    @GetMapping("/")
+    public String hello() {
+        return "welcome to new york";
+    }
 }
