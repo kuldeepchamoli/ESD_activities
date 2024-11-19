b2950b04bd8920f58752ba062e54fae0e8b98e2d
kuldeepchamoli <52848103+kuldeepchamoli@users.noreply.github.com>
2024-11-19 10:59:08 +0530

    Update firstname

--- Yummy_REST-main/src/main/java/org/myprojects/yummy_rest/service/CustomerService.java
+++ Yummy_REST-main/src/main/java/org/myprojects/yummy_rest/service/CustomerService.java
@@ -3,9 +3,12 @@ package org.myprojects.yummy_rest.service;
 import lombok.RequiredArgsConstructor;
 import org.myprojects.yummy_rest.dto.CustomerRequest;
 import org.myprojects.yummy_rest.dto.CustomerResponse;
+import org.myprojects.yummy_rest.dto.CustomerUpdateRequest;
+import org.myprojects.yummy_rest.dto.LoginRequest;
 import org.myprojects.yummy_rest.entity.Customer;
 import org.myprojects.yummy_rest.exception.CustomerNotFoundException;
 import org.myprojects.yummy_rest.helper.EncryptionService;
+import org.myprojects.yummy_rest.helper.JWTHelper;
 import org.myprojects.yummy_rest.mapper.CustomerMapper;
 import org.myprojects.yummy_rest.repo.CustomerRepo;
 import org.springframework.stereotype.Service;
@@ -17,8 +20,8 @@ import static java.lang.String.format;
 public class CustomerService {
     private final CustomerRepo repo;
     private final CustomerMapper mapper;
-    private final CustomerRepo customerRepo;
     private final EncryptionService encryptionService;
+    private final JWTHelper jwtHelper;
 
     public String createCustomer(CustomerRequest request) {
         Customer customer = mapper.toCustomer(request);
@@ -29,7 +32,7 @@ public class CustomerService {
     }
 
     public Customer getCustomer(String email) {
-        return customerRepo.findByEmail(email)
+        return repo.findByEmail(email)
             .orElseThrow(() -> new CustomerNotFoundException(
                 format("No customer found with provided email: %s", email)
             ));
@@ -38,5 +41,32 @@ public class CustomerService {
     public CustomerResponse retrieveCustomer(String email) {
         Customer customer = getCustomer(email);
         return mapper.toCustomerResponse(customer);
+    }
+
+    public String updateCustomer(CustomerUpdateRequest customerUpdateRequest, String authorizationHeader) {
+        String token = authorizationHeader.substring(7);
+        String email = jwtHelper.extractEmail(token);
+
+        Customer customer = getCustomer(email);
+
+        if(customerUpdateRequest.firstName() != null)
+            customer.setFirstName(customerUpdateRequest.firstName());
+        if(customerUpdateRequest.lastName() != null)
+            customer.setLastName(customerUpdateRequest.lastName());
+
+        repo.save(customer);
+        return "Customer updated";
+    }
+
+    // ======================================================
+
+    public String loginCustomer(LoginRequest request) {
+        Customer customer = getCustomer(request.email());
+
+        if(!encryptionService.validates(request.password(), customer.getPassword())) {
+            return "Incorrect email or password";
+        }
+
+        return jwtHelper.generateToken(request.email());
     }
 }
